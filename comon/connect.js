a868201ec3e549be9460905c2991f3fff75bcbf6
David Danh <danh.tu@jobsv.net>
2022-10-13 14:54:29 +0700

    check dieu kien orders

--- comon/connect.js
+++ comon/connect.js
@@ -11,6 +11,12 @@ var mysqlConnection = mysql.createConnection({
 
 mysqlConnection.connect(function(err){
     if(err){
+        console.log({
+            host: dotenv.parsed.MYSQL_HOST,
+            user: dotenv.parsed.MYSQL_USER,
+            password: dotenv.parsed.MYSQL_PASSWORD,
+            database: dotenv.parsed.MYSQL_DATABASE
+        })
         console.log(err)
         console.log("ket noi csdl khong thanh cong")
     }
